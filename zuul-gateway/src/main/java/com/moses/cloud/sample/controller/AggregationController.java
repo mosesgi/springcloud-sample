860be0bbf0b2d4080bddb4613eff235ee6d610d2
Moses Ji <mosionji@163.com>
2019-07-15 21:26:36 +0800

    fix

--- zuul-gateway/src/main/java/com/moses/cloud/sample/controller/AggregationController.java
+++ zuul-gateway/src/main/java/com/moses/cloud/sample/controller/AggregationController.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class AggregationController {
     public static final Logger LOGGER = LoggerFactory.getLogger(AggregationController.class);
 
